297c4e638adb94c7f7db8fe1bdc513be40809879
Juan Alberto Reséndiz Arteaga <jresendiz27@gmail.com>
2015-10-24 17:32:39 -0500

    First testing example, methods that fails and pass

---
+++ src/main/groovy/mx/ipn/tdd/bowling/kata/ExampleTest.groovy
@@ -0,0 +1,14 @@
+package mx.ipn.tdd.bowling.kata
+
+/**
+ * Created by alberto on 24/10/15.
+ */
+class ExampleTest {
+    def shouldPass() {
+        return true
+    }
+
+    def shouldFail() {
+        return false;
+    }
+}
---
+++ src/test/groovy/mx/ipn/tdd/bowling/kata/ExampleTestSpec.groovy
@@ -0,0 +1,22 @@
+package mx.ipn.tdd.bowling.kata
+
+import spock.lang.Specification
+
+/**
+ * Created by alberto on 24/10/15.
+ */
+class ExampleTestSpec extends Specification {
+    def "Example test method should pass"() {
+        given:
+        ExampleTest example = new ExampleTest()
+        expect:
+        example.shouldPass()
+    }
+
+    def "Example test method should fail"() {
+        given:
+        ExampleTest example = new ExampleTest()
+        expect:
+        example.shouldFail()
+    }
+}
